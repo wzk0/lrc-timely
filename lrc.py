eeb6d6f2a1119b777a98c9587825d4a82d00d329
wzk0 <729301277@qq.com>
2022-10-04 18:58:23 +0800

    完善了不规范歌词的修复功能,添加线程的守护属性

--- lrc.py
+++ lrc.py
@@ -16,10 +16,10 @@ def main(player,lrc_name,lrc_path,music_path,sleep_time):
 		for l in lrc:
 			if name in l:
 				if name==l.split('.')[0]:
-					print('\n\033[1;36m已找到同名的歌词文件!\033[0m')
+					print('\n\033[1;36m已找到同名的歌词文件!\n\033[0m')
 					ls.append(l)
 				else:
-					print('\n\033[1;36m未找到同名的歌词文件,但找到可能相关的歌词文件!名称为: \033[1;32m'+str(l)+'\033[0m\033[1;36m 将使用该歌词文件!\033[0m')
+					print('\n\033[1;36m未找到同名的歌词文件,但找到可能相关的歌词文件!名称为: \033[1;32m'+str(l)+'\033[0m\033[1;36m 将使用该歌词文件!\n\033[0m')
 					ls.append(l)
 		for m in music:
 			mm=m.split('.')[0]
@@ -80,7 +80,7 @@ def main(player,lrc_name,lrc_path,music_path,sleep_time):
 		if '00:00.00' in ls[0][0]:
 			ls=ls
 		else:
-			ls.insert(0,['00:00.000','\n不规范的歌词文件,已自动修复!'])
+			ls.insert(0,['00:00.000','\n不规范的歌词文件,已自动修复!\n'])
 		do(ls,sleep_time)
 
 	def play(player,file):
@@ -91,22 +91,31 @@ def main(player,lrc_name,lrc_path,music_path,sleep_time):
 		return False
 	if result[-1]==1:
 		music_name=result[0]
-		print('\n\033[1;36m没有找到歌词!\033[0m')
+		print('\n\033[1;36m没有找到歌词!\n\033[0m')
 		play(player,music_path+music_name)
 	if result[-1]==2:
 		music_name=result[1]
 		lrc_name=result[0]
 		t1=Thread(target=begin,args=(lrc_path+lrc_name,''))
 		t2=Thread(target=play,args=(player,music_path+get_good_name(music_name)))
+		t1.daemon = True
+		t2.daemon = True
 		t1.start()
 		t2.start()
-		t1.join()
 		t2.join()
+		if t2.is_alive()!=True:
+			if t1.is_alive():
+				try:
+					t1._stop()
+				except:
+					print('\n\033[1;36m由于不规范的歌词或手动中断了歌曲,已中途退出!\n\033[0m')
+			else:
+				pass
 
 '''
 eg: 
 
-main('cvlc --play-and-exit ','近藤真彦-夕焼けの歌','歌词/','./',0.01)
+main('cvlc --play-and-exit ','Maru-^Nyan_Cat^__Kawaii_Future_Bass_Remix','歌词/','./',0.01)
 
 main(player,lrc_name,lrc_path,music_path,sleep_time)内,
 
@@ -121,4 +130,21 @@ lrc_path是歌词文件所在路径;
 music_path是音频文件所在路径;
 
 sleep_time是歌词提前于时刻出现的时间长度.这个数请尽量小,因为会叠加效果;如果这个数大,说唱歌曲可能出词速度过快.
+
+=======================================================
+
+10.4更新:
+
+* 五处输出添加了换行(19,22,83,94,116);
+
+* 不规范的歌词的再次规范,例如:
+
+[00:00.000] 作曲 : daniwell
+[99:00.00]纯音乐，请欣赏
+
+(太恐怖了...)
+
+* 中断的相关处理;
+
+* 线程属性的规范(daemon守护),这样一来只需要Ctrl C一次即可中途退出.
 '''
